639fa1dc0f47b8087901d3c5e0acdeb8e92e88f6
Fernanda <fernanda.1999@alunos.utfpr.edu.br>
2025-12-08 18:03:05 -0300

    layout principal

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -5,13 +5,27 @@ import { CharacterDetailPage } from "../components/characters/character-detail-p
 import { DeckCard } from "../components/deck/deck-card/deck-card";
 import { DeckDetailsPage } from "../components/deck/deck-details-page/deck-details-page";
 import { DeckListPage } from "../components/deck/deck-list-page/deck-list-page";
+import { Button } from "../shared/button/button";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-layout',
-  imports: [DeckListPage],
+  standalone: true,
+  imports: [Button, RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
 export class Layout {
+
+  appName = 'Star Wars Cards';
+  favoriteCount: number = 5;
+
+  goToDecks() {
+    // Navigation logic to go to decks page
+  }
+
+  goHome() {  
+    // Navigation logic to go to home page
+  }
 
 }
