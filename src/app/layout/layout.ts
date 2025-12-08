b7a2ea1e162fc07bf28a03abfbfa97973dd5c036
Fernanda <fernanda.1999@alunos.utfpr.edu.br>
2025-12-08 16:23:35 -0300

    layout do componente character details

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CharacterCard } from "../components/characters/character-card/character-card";
 import { CharacterListPage } from "../components/characters/character-list-page/character-list-page";
+import { CharacterDetailPage } from "../components/characters/character-detail-page/character-detail-page";
 
 @Component({
   selector: 'app-layout',
-  imports: [CharacterListPage],
+  imports: [CharacterDetailPage],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
