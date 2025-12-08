1bec25d7dd07f01b1762a249aca4972be6d65532
Fernanda <fernanda.1999@alunos.utfpr.edu.br>
2025-12-08 18:18:28 -0300

    criação das rotas do sistema

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
+import { CharacterListPage } from './components/characters/character-list-page/character-list-page';
+import { DeckListPage } from './components/deck/deck-list-page/deck-list-page';
+import { DeckDetailsPage } from './components/deck/deck-details-page/deck-details-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Layout,
+        children: [
+            { path: '', component: CharacterListPage,},
+            { path: 'decks', component: DeckListPage,},
+            { path: 'decks/:id', component: DeckDetailsPage,},
+            { path: '**', redirectTo: '', pathMatch: 'full' },
+        ],
+    },
+];
